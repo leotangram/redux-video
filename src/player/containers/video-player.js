ff8dbd2aac744687b6e7aa4d47af1eba9b1df990
leotangram <leotangram@gmail.com>
2018-07-30 20:41:27 -0500

    Volume control

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -7,6 +7,7 @@ import Timer from '../components/timer'
 import VideoPlayerControls from '../components/video-player-controls'
 import ProgressBar from '../components/progress-bar';
 import Spinner from '../components/spinner';
+import Volume from '../components/volume'
 
 class VideoPlayer extends Component {
   state = {
@@ -49,6 +50,9 @@ class VideoPlayer extends Component {
       loading: false
     })
   }
+  handleVolumeChange = event => {
+    this.video.volume = event.target.value
+  }
   render() {
     return (
       <VideoPlayerLayout>
@@ -68,6 +72,9 @@ class VideoPlayer extends Component {
             duration={this.state.duration}
             value={this.state.currentTime}
             handleProgressChange={this.handleProgressChange}
+          />
+          <Volume
+            handleVolumeChange={this.handleVolumeChange}
           />
         </VideoPlayerControls>
         <Spinner
