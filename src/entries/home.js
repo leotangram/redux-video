a0e01a4758e5644695b74f39b13759827c4ab0f6
leotangram <leotangram@gmail.com>
2018-08-08 20:46:09 -0500

    Adding a Store to Tangram Video

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,12 +2,27 @@ import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
-import data from "../api.json";
+import data from '../api.json';
+// console.log('Hola mundo!' )
 
-// console.log('Hola mundo!')
+import { createStore } from 'redux';
+
+const initialState = {
+  data: {
+    ...data
+  }
+}
+
+const store = createStore(
+  (state) => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store.getState());
 
 const homeContainer = document.getElementById('home-container')
 
-// ReactDOM.render(que voy a renderizar, donde lo haré)
-// const holaMundo = <h1>Hola mundo!</h1>
-render(<Home data={data} />, homeContainer)
+// ReactDOM.render(que voy a renderizar, donde lo haré);
+// const holaMundo = <h1>hola Estudiante!</h1>;
+render( <Home data={data} />, homeContainer);
