3b4ec196d3d5e2aa48f0f485108e7d9bde3800a4
leotangram <leotangram@gmail.com>
2018-08-13 19:50:58 -0500

    Adding the filtered data to the UI

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,11 +1,18 @@
 function data(state, action) {
   switch (action.type) {
-    case "SEARCH_VIDEO": {
-      // action.payload.query
-      const list = state.data.categories[2].playlist; 
-      const results = list.filter((item) => {
-        return item.author.includes(action.payload.query)
-      })
+    case'SEARCH_VIDEO': {
+      let results = []
+      if (action.payload.query) {
+        state.data.categories.map( category => {
+          results = results.concat(category.playlist)
+        })
+        results = results.filter( item => {
+          const author = item.author.toLowerCase()
+          const query = action.payload.query.toLowerCase()
+          return author.includes(query)
+        })
+      }
+
       return {
         ...state,
         search: results
